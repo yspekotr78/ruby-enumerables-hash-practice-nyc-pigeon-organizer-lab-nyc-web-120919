1e5b9f757e3c5716d0c27523e63718797bb52634
Yury Spektor <yspektor78@gmail.com>
2019-12-01 16:20:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,6 +17,21 @@ require 'pp'
      "City Hall" => ["Andrew"]
    }
  }
+ 
+ def nyc_pigeon_organizer(data)
+  organized_pigeons = {}
+ 	 data.each do |attribute, info|
+     info.each do |detail, names|
+       names.each do |name|
+        organized_pigeons[name] ||= {}
+        organized_pigeons[name][attribute] ||= []
+        organized_pigeons[name][attribute] << detail.to_s
+       end
+     end
+   end
+  organized_pigeons
+end
+
 
 def nyc_pigeon_organizer(data)
   new_hash = {}
