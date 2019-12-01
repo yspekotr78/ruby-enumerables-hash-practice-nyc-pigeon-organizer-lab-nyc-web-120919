355627b3b50dc911dde5bd6d7d18a97b9adfc149
Yury Spektor <yspektor78@gmail.com>
2019-12-01 16:06:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,21 +17,8 @@ require 'pp'
 #     "City Hall" => ["Andrew"]
 #   }
 # }
-new_hash = {}
-end 	  data.each do |attribute, hash|
-    hash.each do |key, array|
-      array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
-        end
-        if !new_hash[name].has_key?(attribute)
-          new_hash[name][attribute] = []
-        end
-        if !new_hash[name][attribute].include?(key)
-          new_hash[name][attribute] << key.to_s
-        end
-      end
-    end
-  end
-  new_hash
+
+def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  pp data
 end
